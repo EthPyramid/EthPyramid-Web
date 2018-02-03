af011404c78f0e1a70273640490fadede8789124
Trendos <36027189+Trendos@users.noreply.github.com>
2018-02-03 13:20:49 +1000

    Coin page
    Test on beta first. Unsure if the USDT will work.

--- js/poh.js
+++ js/poh.js
@@ -392,6 +392,17 @@ function convertWeiToEth(e) {
     return e / 1e18
 }
 
+var ethPrice = 0;
+
+function updateEthPrice() {
+	$.getJSON( 'https://api.coinmarketcap.com/v1/ticker/ethereum/', function( result ) {
+		var eth = result[0];
+		ethPrice = parseFloat( eth.price_usd );
+		setTimeout( updateEthPrice, 10000 );
+	});
+}
+
+updateEthPrice();
 
 function updateData(contract) {
     // Populate data
@@ -403,21 +414,24 @@ function updateData(contract) {
         $('.current-sale .poh-balance').text((r / 1e18*1000).toFixed(4) + " EPY");
         contract.getEtherForTokens(r, function(e, r) {
             $(".current-sale .poh-value").text(convertWeiToEth(r * 0.9).toFixed(4) + " ETH");
+			$(".current-sale .usd-value").text((convertWeiToEth(r * 0.9) * ethPrice).toFixed(4) + " USD");
         })
     })
     contract.buyPrice(function(e, r) {
-        let buyPrice = (1/(convertWeiToEth(r) * .9)/1000000).toFixed(6)
-        $('.current-sale .poh-buy').text(buyPrice + " ETH");
-        
+        let buyPrice = (1/(convertWeiToEth(r) * .9)/1000000);
+        $('.current-sale .poh-buy').text(buyPrice.toFixed(6) + " ETH");
+		$(".current-sale .usd-buy").text((buyPrice * ethPrice).toFixed(6) + " USD");        
     })
 
     contract.sellPrice(function(e, r) {
-        let sellPrice = convertWeiToEth(r).toFixed(6)
-        $('.current-sale .poh-sell').text(sellPrice + " ETH");
+        let sellPrice = convertWeiToEth(r);
+        $('.current-sale .poh-sell').text(sellPrice.toFixed(6) + " ETH");
+		$(".current-sale .usd-sell").text((sellPrice * ethPrice).toFixed(4) + " USD");
     })
 
     contract.dividends(web3.eth.defaultAccount, function(e, r) {
         $('.current-sale .poh-div').text(convertWeiToEth(r).toFixed(4) + " ETH");
+		$(".current-sale .usd-div").text((convertWeiToEth(r) * ethPrice).toFixed(6) + " USD");
     } )
 
     web3.eth.getBalance(contract.address, function(e, r) {
