d15f4f01a52abc5667e71bca8be86b0b97b01bcc
Kane Wallmann <kane@wallmann.ws>
2018-02-10 10:42:02 +1000

    Work on better localisation

---
+++ include/faq.php
@@ -0,0 +1,33 @@
+<div id="token-sale" class="token-main">
+	<div class="container">
+		<div class="token-content">
+			<div class="faq-main">
+				<div class="faq-main-content" style="padding-top: 0px;">
+					<div class="container">
+						<div class="sections">
+							<h4 id="1"><?php __('Q: Is this a real thing?'); ?></h4>
+							<p><?php __('A: Yes, EthPyramid Tokens are built on the Ethereum blockchain. They have no value outside of engaging in the EP game with our Math Robot though and you SHOULD NOT consider them a real investment or asset class with any intrinsic value OF ANY FORM.'); ?></p>
+						</div>
+						<div class="sections">
+							<h4 id="3"><?php __('Q: Wasn\'t there something similar to this earlier?'); ?></h4>
+							<p><?php __('A: Yes, there are several variations of ponzis, pyramids, and other schemes; EthPyramid is the next step forward with convertible dividends and 10% redistribution on sell.'); ?></p>
+						</div>
+						<div class="sections">
+							<h4 id="4"><?php __('Q: Is EthPyramid secure?'); ?></h4>
+							<p><?php __('Yes. Our dev team put a lot of time into refining and testing this contract to make sure your tokens are safe. Internal functions of the contract are not accessible to the end user.'); ?>
+							<p>
+						</div>
+						<div class="sections">
+							<h4 id="5"><?php __('Q: What do I need to buy into this Pyramid?'); ?></h4>
+							<p><?php __('A: You need to install <a href="https://metamask.io/">Metamask</a> and also need to buy some Ether (ETH), probably from something like Coinbase.'); ?></p>
+						</div>
+						<div class="sections">
+							<h4 id="6"><?php __('Q: Is this coin pre-mined?'); ?></h4>
+							<p><?php __('A: No. Nor are there any loops in the code that allow us to take Ether out of the contract in any ways that you yourself are not capable of.'); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
